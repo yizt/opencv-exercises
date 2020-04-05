53d6b8df841988420b4a4906a3adcaf83bab6356
yizt <csuyzt@163.com>
2020-04-05 21:49:19 +0800

    update sth

--- ocr/text_detctor.py
+++ ocr/text_detctor.py
@@ -14,15 +14,21 @@ def main(img):
     vis = img.copy()
     # mser 检测最大稳定极值区域
     mser = cv2.MSER_create()
-    regions = mser.detectRegions(gray)
+    msers, boxes = mser.detectRegions(gray)
 
-    hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
+    # 画凸包
+    hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in msers]
     cv2.polylines(vis, hulls, 1, (0, 255, 0))
+
+    # 画矩形框
+    for box in boxes:
+        x, y, w, h = box
+        cv2.rectangle(vis, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     cv2.imshow('img', vis)
     cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-    image = cv2.imread('../images/tilt02.jpg')
+    image = cv2.imread('../tmp/efg.jpg')
     main(image)
